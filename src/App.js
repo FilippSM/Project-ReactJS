449dba72c6f80e1d7af885345a2af022dc5814fc
Filipp Sam <gagarin080@yandex.ru>
2022-12-18 12:09:13 +0300

    route, browser-router, routing

--- src/App.js
+++ src/App.js
@@ -3,19 +3,28 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Dialogs from "./components/Dialogs/Dialogs";
-
+import News from "./components/News/News";
+import Music from "./components/Music/Music";
+import Setting from "./components/Setting/Setting";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 const App = (props) => {
     return (
-        <div className='app-wrapper'>
-            <Header/>
-            <Navbar/>
-
-            <div class='app-wrapper-content'>
-                <Dialogs />
+        <BrowserRouter>
+            <div className='app-wrapper'>
+                <Header/>
+                <Navbar/>
+                <div class='app-wrapper-content'>
+                    <Routes>
+                        <Route path='/dialogs' element={<Dialogs/>}/>
+                        <Route path='/profile' element={<Profile/>}/>
+                        <Route path='/news' element={<News/>}/>
+                        <Route path='/music' element={<Music/>}/>
+                        <Route path='/setting' element={<Setting/>}/>
+                    </Routes>
+                </div>
             </div>
-            {/*<Profile />*/}
-        </div>
+        </BrowserRouter>
     );
 }
 
