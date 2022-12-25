2b1ef2c05c12d36d6cba2928eabc2ba57536ff16
Filipp Sam <gagarin080@yandex.ru>
2022-12-25 14:00:10 +0300

    dialogs layout, changle styles

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,8 +2,35 @@ import s from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Dimych
+                </div>
+                <div className={s.dialog}>
+                    Illidan
+                </div>
+                <div className={s.dialog}>
+                    Rogan
+                </div>
+                <div className={s.dialog}>
+                    Garosh
+                </div>
+                <div className={s.dialog}>
+                    Jupiter
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.dialog}>
+                    Hi.
+                </div>
+                <div className={s.dialog}>
+                    What's new?
+                </div>
+                <div className={s.dialog}>
+                    By.
+                </div>
+            </div>
         </div>
     )
 }
